baf960f3340e25f301439da675daddd3b7265b77
Solowise130 <iniodusolomon@gmail.com>
2023-08-23 10:25:32 +0000

    0. Change comes from within.

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Create an array to store results of subproblems
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Fill dp[] in a bottom up manner
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # If total cannot be made up by any combination of the coins
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
+
+
+# Example usage
+coins = [1, 5, 10, 25]
+total = 37
+print(makeChange(coins, total))  # This should print 4 (25 + 10 + 1 + 1)
