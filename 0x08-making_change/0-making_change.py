6f3f4d40ce13723df0a7a1db32623b88056c4e98
Solowise130 <iniodusolomon@gmail.com>
2023-08-23 10:32:45 +0000

    0. Change comes from within.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,7 +8,7 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    # Fill dp[] in a bottom up manner
+    # Fill dp[] in a bottom-up manner
     for i in range(1, total + 1):
         for coin in coins:
             if i >= coin:
@@ -19,9 +19,3 @@ def makeChange(coins, total):
         return -1
 
     return dp[total]
-
-
-# Example usage
-coins = [1, 5, 10, 25]
-total = 37
-print(makeChange(coins, total))  # This should print 4 (25 + 10 + 1 + 1)
