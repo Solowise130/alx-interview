ae8f21e985baa9bac2973e7e851a4feb61624f67
Solowise130 <iniodusolomon@gmail.com>
2023-09-07 09:27:34 +0000

    0. Prime Game.

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,34 @@
 #!/usr/bin/python3
+"""Given consecutive integers starting from 1 up to and including n"""
 
-def is_prime(n):
-    """Check if a number is prime."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def play_round(n):
-    """Play one round of the Prime Game and return the winner's name."""
-    nums = list(range(2, n+1))
-    turn = 0  # 0 for Maria, 1 for Ben
-
-    while True:
-        found_prime = False
-        for i in nums:
-            if is_prime(i):
-                found_prime = True
-                multiples = list(range(i, n+1, i))
-                nums = [x for x in nums if x not in multiples]
-                break
-
-        if not found_prime:
-            return "Maria" if turn == 1 else "Ben"
-
-        turn = 1 - turn  # switch turns
 
 def isWinner(x, nums):
-    """Determine the winner of x rounds of the Prime Game."""
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        winner = play_round(n)
-        if winner == "Maria":
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    """Given consecutive integers starting from 1 up to and including n"""
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
+
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
+
+    winner = ''
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        winner = None
+    if player1 * 2 > len(nums):
+        winner = "Maria"
+    else:
+        winner = "Ben"
+    return winner
